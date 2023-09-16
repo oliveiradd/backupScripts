04201806d7e389b017f5e250442cc51a36bc9a69
Daniel <oliveira.d.c@outlook.com>
2023-09-15 22:23:55 -0300

    modified:   Nextcloud/backupScript.cmd

--- Windows/Nextcloud/backupScript.cmd
+++ Windows/Nextcloud/backupScript.cmd
@@ -5,7 +5,7 @@ set destination=%userprofile%\Nextcloud\%computername%\%username%
 set options=/E /R:3 /W:5 /XA:H /XJD /XD AppData Contacts Downloads Favorites IntelGraphicsProfiles Links Onedrive Nextcloud Searches /XF *.DAT* *ntuser* desktop.ini
 
 if not exist %userprofile%\Nextcloud ( 
-	msg %username% "Error. Nextcloud default folder not found."
+	msg "%username%" "Error. Nextcloud default folder not found."
 	exit
 )
 
@@ -14,13 +14,13 @@ if not exist %userprofile%\Nextcloud\%computername%\%username% (
 )
 
 if %1==--add (
-    robocopy %source%\ %destination%\ %options% /XO
+    robocopy "%source%" "%destination%" %options% /XO
 ) else if %1==--sync (
-    robocopy %source%\ %destination%\ %options% /XO /PURGE
+    robocopy "%source%" "%destination%" %options% /XO /PURGE
 ) else if %1==--pull (
-    robocopy %destination%\ %source%\ %options% /XO /PURGE
+    robocopy "%destination%" "%source%" %options% /XO
 ) else if %1==--restore (
-    robocopy %destination%\ %source%\ %options% /PURGE
+    robocopy "%destination%" "%source%" %options%
 ) else (
     echo "Option not recognized."
 )

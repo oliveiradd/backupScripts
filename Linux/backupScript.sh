6d32b4da08ce031c1b8d260f7eb1d7733f6b752d
Daniel <oliveira.d.c@outlook.com>
2024-01-27 18:17:04 -0300

    make script pick folders from a text file to backup

--- Linux/backupScript.sh
+++ Linux/backupScript.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## file with list of directories to backup
+script_path=$(realpath "$0")
+script_dir=$(dirname "$script_path")
+dir_list=$script_dir/targets
+
 ## remote parameters
 remote_user=$USER
 server=10.8.0.1
@@ -11,12 +16,6 @@ DBackup1
 DBackup2
 )
 
-## general parameters
-additional_sources=(
-#relative path from $HOME
-.ssh
-.kiss
-)
 devices_mount_path=/run/media/$USER
 samba_mount_point=/run/user/$UID/gvfs/smb-share:server=$server,share=$remote_user
 openvpn_filename=thinkpad-noredir
@@ -26,7 +25,8 @@ mode=$3
 additional_option=$4
 
 #rsync_options="-rtuv --no-links"
-rsync_options="-auv"
+#rsync_options="-auv"
+rsync_options="-rltgoDuv"
 
 ## backup function
 backup_data() {
@@ -40,17 +40,9 @@ backup_data() {
 	fi
 
 	if [[ $1 = --send ]]; then
-		rsync $rsync_options --exclude={'.*','Downloads','Public','Templates'} $mode2 $3 $HOME/ $external_storage
-		rsync $rsync_options --exclude={'Downloads','Public','Templates','.vault'} $mode2 $3 $HOME/* $external_storage
-		for item in ${additional_sources[@]}; do
-			rsync $rsync_options $mode2 $3 $HOME/$item/ $external_storage/$item
-		done
+		rsync --mkpath $rsync_options --exclude={'.*','Downloads','Public','Templates'} $mode2 $3 $path/ $external_storage$path/
 	elif [[ $1 = --receive ]]; then
-		rsync $rsync_options --exclude={'.*','Downloads','Public','Templates'} --force $mode2 $3 $external_storage/ $HOME
-		rsync $rsync_options --exclude={'Downloads','Public','Templates','.vault'} --force $mode2 $3 $external_storage/* $HOME
-		for item in ${additional_sources[@]}; do
-			rsync $rsync_options --force $mode2 $3 $external_storage/$item/ $HOME/$item
-		done
+		rsync --mkpath $rsync_options --exclude={'.*','Downloads','Public','Templates'} --force $mode2 $3 $external_storage$path/ $path
 	else
 		echo "Primeiro argumento não reconhecido."
 		exit 1
@@ -71,14 +63,10 @@ snapshot() {
 ## local transfer function
 local_transfer() {
 for partition_name in ${backup_partitions[@]}; do
-	external_storage=$devices_mount_path/$partition_name/$USER
+	external_storage=$devices_mount_path/$partition_name/$HOSTNAME
 	if [[ -d $external_storage ]]; then
 		backup_data $action $mode $additional_option
-		if [[ $additional_option == "--dry-run" ]]; then
-			echo "Opção dry run: snapshot não será criado"
-			exit 0
-		fi
-		snapshot
+        break
 	fi
 done
 }
@@ -104,22 +92,28 @@ fi
 
 ## rsync daemon transfer function 
 rsync_transfer() {
-	external_storage=$remote_user@$server::danielbkp/
+	external_storage=$remote_user@$server::danielbkp
 	backup_data $action $mode $additional_option
 }
 
 ## Start of script
-if [[ $target = --local ]]; then
-	local_transfer
-elif [[ $target = --samba ]]; then
-	samba_transfer
-elif [[ $target = --rsyncd ]]; then
-#	vpn
-#	sleep 10
-	rsync_transfer
-#	vpn --disconnect
-else 
-	echo "Error: invalid target value"
+while IFS= read -r path; do
+    if [[ $target = --local ]]; then
+        local_transfer
+    elif [[ $target = --samba ]]; then
+        samba_transfer
+    elif [[ $target = --rsyncd ]]; then
+    #	vpn
+    #	sleep 10
+        rsync_transfer
+    #	vpn --disconnect
+    else 
+        echo "Error: invalid target value"
+    fi
+done < $dir_list
+
+if [[ $additional_option != "--dry-run" && $target == "--local" ]]; then
+    snapshot
 fi
 ## End of script
 

83ab096573f1b6f6c42ae9cf09bbde72aca165b6
Daniel <oliveira.d.c@outlook.com>
2024-03-22 00:33:09 -0300

    rename backup script and add restore

---
+++ Windows/SMB/restore.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+if not exist "%~dp0config\config.txt" (
+	echo File^ config.txt^ not^ found!
+	pause
+	exit
+)
+for /F "usebackq tokens=*" %%V in ( `type "%~dp0config\config.txt" ^| findstr /V "^#"` ) do ( set "%%V" )
+
+set "destination=\\%server_ip%\%server_share%"
+
+if not exist "%destination%" ( 
+	echo Error:^ network^ share^ not^ available.
+	exit
+)
+
+if not exist "%~dp0config\targets.txt" (
+	echo File^ targets.txt^ not^ found!
+	pause
+	exit
+)
+
+if "%1"=="--sync" (
+    set options=/S /PURGE /R:3
+) else (
+    set options=/S /R:3
+)
+set exclude=/XF *.DAT* *ntuser* desktop.ini
+
+for /F "usebackq tokens=*" %%V in ( `type "%~dp0config\targets.txt" ^| findstr /V "^#"` ) do ( robocopy "%destination%\%computername%\%username%\%%V" "%userprofile%\%%V" %options% %exclude% )
+pause

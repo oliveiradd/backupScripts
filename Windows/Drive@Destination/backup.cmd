1619b37f3d3bfb24bd4fe1ebe5a33c35a0eb1b2e
Daniel <oliveira.d.c@outlook.com>
2024-03-23 13:10:51 -0300

    fix broken scripts

--- Windows/Drive@Destination/backup.cmd
+++ Windows/Drive@Destination/backup.cmd
@@ -1,17 +1,5 @@
 @echo off
 
-if not exist "%~dp0config\config.txt" (
-	echo File^ config.txt^ not^ found!
-	pause
-	exit
-)
-for /F "usebackq tokens=*" %%V in ( `type "%~dp0config\config.txt" ^| findstr /V "^#"` ) do ( set "%%V" )
-
-if not exist "%destination%" ( 
-	echo Error:^ drive^ not^ available.
-	exit
-)
-
 if not exist "%~dp0config\targets.txt" (
 	echo File^ targets.txt^ not^ found!
 	pause
